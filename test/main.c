73d07ec9f9287f6ad1a772650f5d409e33d2d48e
Emil Brink <emil@obsession.se>
2014-05-23 20:47:09 +0200

    Added test code for the size encoding API, and a Makefile.
    It doesn't work, though. :)

--- test/main.c
+++ test/main.c
@@ -2,6 +2,8 @@
  *
 */
 
+#include <stdio.h>
+
 #include "lzjb-stream.h"
 
 /* Load the given file. Doesn't just use mmap() because reasons. */
@@ -39,8 +41,33 @@ static void * load_file(const char *filename, size_t *length)
 
 int main(int argc, char *argv[])
 {
-	LZJBStream	s;
-	char		in[128], out[128];
+	printf("Testing lzjb-stream's size codec API ...\n");
+	for(size_t i = 0; i < 10000; ++i)
+	{
+		char			buf[8];
+		const size_t	size = rand();
 
-	lzjbstream_init(&s, dst_size, in, sizeof in);
+		void *encode_end = lzjbstream_size_encode(buf, sizeof buf, size);
+		if(encode_end == NULL)
+		{
+			printf("Failed to encode %zu, out of buffer space\n", size);
+			return EXIT_FAILURE;
+		}
+		const size_t encoded_length = (char *) encode_end - buf;
+		size_t size_out;
+		const void *decode_end = lzjbstream_size_decode(buf, encoded_length, &size_out);
+		if(decode_end == NULL)
+		{
+			printf("Failed to decode %zu from %zu bytes of encoded data, ran out of input\n", size, encoded_length);
+			return EXIT_FAILURE;
+		}
+		if(size != size_out)
+		{
+			printf("Failed to decode %zu, got %zu instead (from %zu bytes)\n", size, size_out, encoded_length);
+			return EXIT_FAILURE;
+		}
+		printf("%zu OK (%zu)\n", size, i);
+	}
+	printf("OK\n");
+	return EXIT_SUCCESS;
 }
