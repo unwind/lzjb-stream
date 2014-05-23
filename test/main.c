45a57367668d65a613e0170644baad414755ab98
Emil Brink <emil@obsession.se>
2014-05-23 20:34:02 +0200

    Added some file-reading code.
    Copy-pasted from python-lzjb's cdriver.c program. Not that anyone cares.

--- test/main.c
+++ test/main.c
@@ -4,6 +4,43 @@
 
 #include "lzjb-stream.h"
 
+/* Load the given file. Doesn't just use mmap() because reasons. */
+static void * load_file(const char *filename, size_t *length)
+{
+	FILE	*in = fopen(filename, "rb");
+	void	*buf = NULL;
+
+	if(in != NULL)
+	{
+		if(fseek(in, 0, SEEK_END) >= 0)
+		{
+			const long size = ftell(in);
+			if(size >= 0)
+			{
+				if(length != NULL)
+					*length = (size_t) size;
+				if(fseek(in, 0, SEEK_SET) == 0)
+				{
+					if((buf = malloc(size)) != NULL)
+					{
+						if(fread(buf, size, 1, in) != 1)
+						{
+							free(buf);
+							buf = NULL;
+						}
+					}
+				}
+			}
+		}
+		fclose(in);
+	}
+	return buf;
+}
+
 int main(int argc, char *argv[])
 {
+	LZJBStream	s;
+	char		in[128], out[128];
+
+	lzjbstream_init(&s, dst_size, in, sizeof in);
 }
