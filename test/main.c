36c7a6a4b7aa31b2f6198dde1e6aecb6925e293e
Emil Brink <emil@obsession.se>
2014-05-24 11:36:06 +0200

    Refactored test code, added some edge cases.

--- test/main.c
+++ test/main.c
@@ -39,35 +39,47 @@ static void * load_file(const char *filename, size_t *length)
 	return buf;
 }
 
+static bool test_size(size_t size)
+{
+	char	buf[32];
+
+	void * const encode_end = lzjbstream_size_encode(buf, sizeof buf, size);
+	if(encode_end == NULL)
+	{
+		printf("Failed to encode %zu, out of buffer space\n", size);
+		return false;
+	}
+	const size_t encoded_length = (char *) encode_end - buf;
+	size_t size_out;
+	const void * const decode_end = lzjbstream_size_decode(buf, encoded_length, &size_out);
+	if(decode_end == NULL)
+	{
+		printf("Failed to decode %zu from %zu bytes of encoded data, ran out of input\n", size, encoded_length);
+		return false;
+	}
+	if(size != size_out)
+	{
+		printf("Failed to decode %zu, got %zu instead (from %zu bytes)\n", size, size_out, encoded_length);
+		return false;
+	}
+	return true;
+}
+
 int main(int argc, char *argv[])
 {
 	printf("Testing lzjb-stream's size codec API ...\n");
-	for(size_t i = 0; i < 10000; ++i)
+	/* Hard-coded edge cases. */
+	test_size(0);
+	test_size(~(size_t) 0);
+	/* A single walking 1-bit. */
+	for(size_t i = 1; i != 0; i <<= 1)
+		test_size(i);
+	for(size_t i = 0; i < 1000000; ++i)
 	{
-		char			buf[8];
 		const size_t	size = rand();
 
-		void * const encode_end = lzjbstream_size_encode(buf, sizeof buf, size);
-		if(encode_end == NULL)
-		{
-			printf("Failed to encode %zu, out of buffer space\n", size);
-			return EXIT_FAILURE;
-		}
-		const size_t encoded_length = (char *) encode_end - buf;
-		size_t size_out;
-		const void * const decode_end = lzjbstream_size_decode(buf, encoded_length, &size_out);
-		if(decode_end == NULL)
-		{
-			printf("Failed to decode %zu from %zu bytes of encoded data, ran out of input\n", size, encoded_length);
-			return EXIT_FAILURE;
-		}
-		if(size != size_out)
-		{
-			printf("Failed to decode %zu, got %zu instead (from %zu bytes)\n", size, size_out, encoded_length);
-			return EXIT_FAILURE;
-		}
-		printf("%zu OK (%zu)\n", size, i);
 	}
 	printf("OK\n");
+
 	return EXIT_SUCCESS;
 }
