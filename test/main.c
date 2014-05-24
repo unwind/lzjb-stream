6dc7118e36b5fcaa794d7df08c6a0a128c5c383c
Emil Brink <emil@obsession.se>
2014-05-24 11:42:24 +0200

    More structure around tests, also count the tests.

--- test/main.c
+++ test/main.c
@@ -2,9 +2,31 @@
  *
 */
 
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "lzjb-stream.h"
+
+static struct {
+	size_t	count;		/* Number of tests we've done. */
+	size_t	pass_count;
+	char	fail[256];
+} test_state;
+
+static void test_passed(void)
+{
+	++test_state.count;
+	++test_state.pass_count;
+}
+
+static void test_failed(const char *fmt, ...)
+{
+	++test_state.count;
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(test_state.fail, sizeof test_state.fail, fmt, args);
+	va_end(args);
+}
 
 /* Load the given file. Doesn't just use mmap() because reasons. */
 static void * load_file(const char *filename, size_t *length)
@@ -39,30 +61,30 @@ static void * load_file(const char *filename, size_t *length)
 	return buf;
 }
 
-static bool test_size(size_t size)
+static void test_size(size_t size)
 {
 	char	buf[32];
 
 	void * const encode_end = lzjbstream_size_encode(buf, sizeof buf, size);
 	if(encode_end == NULL)
 	{
-		printf("Failed to encode %zu, out of buffer space\n", size);
-		return false;
+		test_failed("Failed to encode %zu, out of buffer space\n", size);
+		return;
 	}
 	const size_t encoded_length = (char *) encode_end - buf;
 	size_t size_out;
 	const void * const decode_end = lzjbstream_size_decode(buf, encoded_length, &size_out);
 	if(decode_end == NULL)
 	{
-		printf("Failed to decode %zu from %zu bytes of encoded data, ran out of input\n", size, encoded_length);
-		return false;
+		test_failed("Failed to decode %zu from %zu bytes of encoded data, ran out of input\n", size, encoded_length);
+		return;
 	}
 	if(size != size_out)
 	{
-		printf("Failed to decode %zu, got %zu instead (from %zu bytes)\n", size, size_out, encoded_length);
-		return false;
+		test_failed("Failed to decode %zu, got %zu instead (from %zu bytes)\n", size, size_out, encoded_length);
+		return;
 	}
-	return true;
+	test_passed();
 }
 
 int main(int argc, char *argv[])
@@ -76,10 +98,9 @@ int main(int argc, char *argv[])
 		test_size(i);
 	for(size_t i = 0; i < 1000000; ++i)
 	{
-		const size_t	size = rand();
-
+		test_size(rand());
 	}
-	printf("OK\n");
+	printf("%zu/%zu tests passed\n", test_state.pass_count, test_state.count);
 
 	return EXIT_SUCCESS;
 }
