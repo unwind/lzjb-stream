d7d3fa4c92bd9f7b8857fe6457d7177e470a2fd5
Emil Brink <emil@obsession.se>
2014-05-29 13:00:46 +0200

    More API design.
    It's kind of hard, since I have some very particular use-cases in mind but don't want them to dominate too much.

--- include/lzjb-stream.h
+++ include/lzjb-stream.h
@@ -17,6 +17,10 @@ typedef struct {
 	size_t	dst_size;
 } LZJBStream;
 
+typedef size_t	(*LZJBStreamRead)(size_t offset, void *out, size_t num_bytes, void *user);
+
+typedef size_t	(*LZJBStreamWrite)(size_t offset, const void *out, size_t num_bytes, void *user);
+
 /* ----------------------------------------------------------------- */
 
 /** @brief Encodes a size using a variable-length format.
@@ -47,18 +51,31 @@ const void * lzjbstream_size_decode(const void *in, size_t in_max, size_t *size)
 
 /* ----------------------------------------------------------------- */
 
-/** @brief Initializes a stream for "full" streaming of both compressed and uncompressed data.
+/** @brief Initializes a stream for "memory" streaming, in which the entire destination buffer
+ * is directly available as actual random-access memory.
  *
  * @param stream	The stream to initialize.
- * @param dst_size	Number of destination (uncompressed) bytes that are going to be generated.
+ * @param dst		Destination buffer.
+ * @param dst_size	Size of destination buffer, number of uncompressed bytes to generate.
 */
-bool lzjbstream_init(LZJBStream *stream, size_t dst_size);
+bool lzjbstream_init_memory(LZJBStream *stream, void *dst, size_t dst_size);
+
+
+/** @brief Initializes a stream for "file" streaming, in which the I/O is deferred to user-supplied callbacks.
+ *
+ * @param stream	The stream to initialize.
+ * @param reader	A user function to read *from the already-decompressed data*.
+ * @param writer	A user function to write newly decompressed data to the destination.
+*/
+bool lzjbstream_init_file(LZJBStream *stream, LZJBStreamRead reader, LZJBStreamWrite writer, void *user);
 
 
 /** @brief Decompress a stream of data.
  *
  * @param stream	The stream to read from.
- * @param buf		Buffer to read into, will be filled with decompressed data.
- * @param buf_size	Maximum number of bytes that @ref buf will hold.
-*/
-size_t lzjbstream_decompress(LZJBStream *stream, const void *src, size_t src_size, void *buf, size_t buf_max);
+ * @param src		Compressed bytes to decompress.
+ * @param src_size	Number of compressed bytes available at @ref src.
+ * @return The number of compressed bytes at @ref src that were processed. If this value is less than
+ *  @ref src_size, the remainder must be kept in the buffer and included in a later call to @ref lzjbstream_decompress().
+ /
+size_t lzjbstream_decompress(LZJBStream *stream, const void *src, size_t src_size);
