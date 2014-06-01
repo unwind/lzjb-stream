39901cc7dbe840b48de13354590de01cc50d375e
Emil Brink <emil@obsession.se>
2014-06-01 20:43:13 +0200

    More Doxygen polish.
    Note: easier to validate this if I actually set up a Doxyfile.

--- include/lzjb-stream.h
+++ include/lzjb-stream.h
@@ -44,7 +44,7 @@ void * lzjbstream_size_encode(void *out, size_t out_max, size_t size);
  * @param in		Buffer from which the encoded size will be read.
  * @param in_max	Maximum number of bytes available to read.
  * @param size		Pointer to location where the final size will be stored.
- *					If @c NULL, no size is stored but the decoding still happens.
+ *
  * @return Pointer to first byte in @ref in after the encoded size.
 */
 const void * lzjbstream_size_decode(const void *in, size_t in_max, size_t *size);
@@ -57,6 +57,8 @@ const void * lzjbstream_size_decode(const void *in, size_t in_max, size_t *size)
  * @param stream	The stream to initialize.
  * @param dst		Destination buffer.
  * @param dst_size	Size of destination buffer, number of uncompressed bytes to generate.
+ *
+ * @return @c true on success, @c false on error (one or more parameter had an invalid value).
 */
 bool lzjbstream_init_memory(LZJBStream *stream, void *dst, size_t dst_size);
 
@@ -66,6 +68,8 @@ bool lzjbstream_init_memory(LZJBStream *stream, void *dst, size_t dst_size);
  * @param stream	The stream to initialize.
  * @param reader	A user function to read *from the already-decompressed data*.
  * @param writer	A user function to write newly decompressed data to the destination.
+ *
+ * @return @c true on success, @c false on error (one or more parameter had an invalid value).
 */
 bool lzjbstream_init_file(LZJBStream *stream, LZJBStreamRead reader, LZJBStreamWrite writer, void *user);
 
@@ -75,7 +79,8 @@ bool lzjbstream_init_file(LZJBStream *stream, LZJBStreamRead reader, LZJBStreamW
  * @param stream	The stream to read from.
  * @param src		Compressed bytes to decompress.
  * @param src_size	Number of compressed bytes available at @ref src.
+ *
  * @return The number of compressed bytes at @ref src that were processed. If this value is less than
- *  @ref src_size, the remainder must be kept in the buffer and included in a later call to @ref lzjbstream_decompress().
- /
+ * @ref src_size, the remainder must be kept in the buffer and included in a later call to @ref lzjbstream_decompress().
+*/
 size_t lzjbstream_decompress(LZJBStream *stream, const void *src, size_t src_size);
